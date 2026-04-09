cc01b5fba734774acd7525eed6e20b1e009bcd6b
Alexander Antipov <lpener76@gmail.com>
2026-04-09 20:44:20 +0400

    Установка модулей

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { MantineProvider, createTheme } from '@mantine/core'
 import App from './App.jsx'
+import '@mantine/core/styles.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const theme = createTheme({
+  fontFamily: 'Nunito, sans-serif',
+  primaryColor: 'violet', 
+  defaultRadius: 'lg',
+})
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <MantineProvider theme={theme}>
+      <App />
+    </MantineProvider>
+  </React.StrictMode>,
 )
